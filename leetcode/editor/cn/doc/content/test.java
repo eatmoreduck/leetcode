03d0f53567ae6f0e31813fe68a712d53a65c5426
eatmoreduck <yuxianice@163.com>
2025-10-14 00:08:11 +0800

    add two sum

---
+++ leetcode/editor/cn/doc/content/test.java
@@ -0,0 +1,27 @@
+package doc.content;
+
+/**
+ * @author xy
+ * @date 2025/10/13
+ * @description
+ */
+
+public class test {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = nums.length - 1; j > 0; j--) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+}
